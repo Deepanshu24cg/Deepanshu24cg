fbc1e3aaa8dc0446adb07c9a10409dd3986a62b7
Deepanshu24cg <69116336+Deepanshu24cg@users.noreply.github.com>
2024-04-06 23:33:12 +0530

    Create php.php

---
+++ php.php
@@ -0,0 +1,56 @@
+<?php
+       echo"hii<br>";
+       echo"this is live reload test<br>";
+       echo"and the test is successful and the result is positive";
+       ?> <!DOCTYPE html>
+       <html lang="en">
+       <head>
+              <meta charset="UTF-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <title>Document</title>
+       </head>
+       <body>
+              <button>Press here</button><br>
+              <p>php lesson - 1 ends here</p>
+              <p>Variable = a reusable container that holds data<br>
+              string, integer, float, boolean</p>
+       </body>
+       </html>
+       <?php
+       
+       //variables list
+
+       echo "String variables<br>";
+       $name = "Deepanshu kumar";
+       $interest = "logic errors";
+       $socialmediaid = "deepanshu24cg<br>";
+       $age = 20;
+       $gpa = 2.5;
+       $qty = 3;
+       $price = 51;
+       $employed = true;
+       $for_sale = true;
+       $total = null;
+       $total = $qty * $price;
+
+
+
+       //echo list
+       echo "I {$name}<br>";
+       echo "like {$interest}<br>";
+       echo "My social media id is {$socialmediaid}<br>";
+       echo "Integer variables<br>";
+       echo "My age is {$age}<br>";
+       echo "Float variable<br>";
+       echo "GPA is {$gpa}<br>";
+       echo "Booleans";
+       echo "<br>Online status : {$employed}<br>";
+       echo "For sale : {$for_sale}<br>";
+       echo "The price of Pizza is {$price} for each<br>" ;
+       echo "Your ordered QTY is {$qty}<br>";
+       echo "and your total is {$total}<br>s";
+
+       echo"now we will learn arithemetic operators"
+
+
+?>
